678afba9a3c119a418b24b3ca3c56adffa4ec439
wangkeyan <wangkeyan@qianxin.com>
2022-04-26 17:19:54 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,5 @@
-#!/usr/bin/env sh
-
-# 确保脚本抛出遇到的错误
-set -e
-
-# 生成静态文件
-npm run docs:build
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
 
 git add .
+read commitString
 git commit -m commitString
 git push
-
-cd -
